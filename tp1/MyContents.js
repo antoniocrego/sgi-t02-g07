2446b1dc5a4191a24d3a99fc4977535ca900ca5a
António Rego <up202108666@up.pt>
2024-10-24 04:47:41 +0100

    Changing plane color to red

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -22,11 +22,32 @@ class MyContents  {
         this.boxDisplacement = new THREE.Vector3(0,2,0)
 
         // plane related attributes
-        this.diffusePlaneColor = "#00ffff"
-        this.specularPlaneColor = "#777777"
-        this.planeShininess = 30
-        this.planeMaterial = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
-            specular: this.specularPlaneColor, emissive: "#000000", shininess: this.planeShininess })
+        this.planeTexture =
+        new THREE.TextureLoader().load('textures/feup_b.jpg');
+        this.planeTexture.wrapS = THREE.RepeatWrapping;
+        this.planeTexture.wrapT = THREE.RepeatWrapping;
+ 
+             // material
+        this.diffusePlaneColor =  "rgb(128,0,0)"
+        this.specularPlaneColor = "rgb(0,0,0)"
+        this.planeShininess = 0
+ 
+             // relating texture and material:
+                 // two alternatives with different results
+ 
+                     // alternative 1
+        this.planeMaterial = new THREE.MeshPhongMaterial({
+                color: this.diffusePlaneColor,
+                specular: this.specularPlaneColor,
+                emissive: "#000000", shininess: this.planeShininess,
+                map: this.planeTexture })
+                     // end of alternative 1
+ 
+                     // alternative 2
+         // this.planeMaterial = new THREE.MeshLambertMaterial({
+         //        map : this.planeTexture });
+                     // end of alternative 2
+        let plane = new THREE.PlaneGeometry( 10, 10 );
     }
 
     /**
@@ -73,11 +94,26 @@ class MyContents  {
         
         // Create a Plane Mesh with basic material
         
-        let plane = new THREE.PlaneGeometry( 10, 10 );
+        let planeSizeU = 10;
+        let planeSizeV = 7;
+        let planeUVRate = planeSizeV / planeSizeU;
+
+        let planeTextureUVRate = 3354 / 2385; // image dimensions
+        let planeTextureRepeatU = 1
+        let planeTextureRepeatV =
+            planeTextureRepeatU * planeUVRate * planeTextureUVRate;
+        this.planeTexture.repeat.set(
+            planeTextureRepeatU, planeTextureRepeatV );
+        this.planeTexture.rotation = 0;
+        this.planeTexture.offset = new THREE.Vector2(0,0);
+
+        var plane = new THREE.PlaneGeometry( planeSizeU, planeSizeV );
         this.planeMesh = new THREE.Mesh( plane, this.planeMaterial );
         this.planeMesh.rotation.x = -Math.PI / 2;
-        this.planeMesh.position.y = -0;
+        this.planeMesh.position.y = 0;
         this.app.scene.add( this.planeMesh );
+
+
     }
     
     /**
