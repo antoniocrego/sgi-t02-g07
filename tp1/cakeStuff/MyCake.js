120dc08de70888d89e69d38e308526ae1d462d1b
António Rego <up202108666@up.pt>
2024-10-30 22:39:12 +0000

    fix to cake texture

--- tp1/cakeStuff/MyCake.js
+++ tp1/cakeStuff/MyCake.js
@@ -18,13 +18,13 @@ class MyCake{
         this.cakeTexture = new THREE.TextureLoader().load("textures/cake.avif");
         this.cakeTexture.wrapS = THREE.RepeatWrapping;
         this.cakeTexture.wrapT = THREE.RepeatWrapping;
-        this.cakeTexture.repeat.set(4, 3);
+        this.cakeTexture.repeat.set(2, 2);
         this.cakeMaterial = new THREE.MeshPhongMaterial({ color: "#ffffff", specular: "#000000", emissive: "#000000", shininess: 20, map:  this.cakeTexture});
 
         this.sideCakeTexture = new THREE.TextureLoader().load("textures/cake.avif");
         this.sideCakeTexture.wrapS = THREE.RepeatWrapping;
         this.sideCakeTexture.wrapT = THREE.RepeatWrapping;
-        this.sideCakeTexture.repeat.set(1, 5);
+        this.sideCakeTexture.repeat.set(5, 1);
         this.sideCakeMaterial = new THREE.MeshPhongMaterial({ color: "#ffffff", specular: "#000000", emissive: "#000000", shininess: 20, map:  this.sideCakeTexture});
 
         this.cakeInteriorTexture = new THREE.TextureLoader().load("textures/cakeInterior.webp");
@@ -32,12 +32,10 @@ class MyCake{
         this.cakeInteriorTexture.wrapT = THREE.RepeatWrapping;
         this.cakeInteriorTexture.repeat.set(1, 1);
         this.cakeInteriorMaterial = new THREE.MeshPhongMaterial({ color: "#ffffff", specular: "#000000", emissive: "#000000", shininess: 20, map:  this.cakeInteriorTexture});
-
-        //this.cakeTotalMaterial = new THREE.MeshFaceMaterial([this.cakeMaterial, this.sideCakeMaterial, this.cakeMaterial]);
     }
 
     buildCake(){
-        this.cakeMesh = this.primitives.buildCylinder(0, 0, 0, 0, 0, 0, 1.25, 1.25, 0.8, 30, 5, false, 0, 2*Math.PI-2*Math.PI/10, this.cakeMaterial, false);
+        this.cakeMesh = this.primitives.buildCylinder(0, 0, 0, 0, 0, 0, 1.25, 1.25, 0.8, 30, 5, false, 0, 2*Math.PI-2*Math.PI/10, [this.sideCakeMaterial, this.cakeMaterial, this.cakeMaterial], false);
         let covers = this.primitives.buildCylinderCovers(0, 0, 0, 0, 0, 0, 1.25, 0.8, 0, 2*Math.PI-2*Math.PI/10, this.cakeInteriorMaterial, false);
         this.cakeCover1 = covers[0];
         this.cakeCover2 = covers[1];
--- tp1/cakeStuff/MyCakeSlice.js
+++ tp1/cakeStuff/MyCakeSlice.js
@@ -22,7 +22,7 @@ class MyCakeSlice{
         this.sideCakeTexture = new THREE.TextureLoader().load("textures/cake.avif");
         this.sideCakeTexture.wrapS = THREE.RepeatWrapping;
         this.sideCakeTexture.wrapT = THREE.RepeatWrapping;
-        this.sideCakeTexture.repeat.set(1, 5);
+        this.sideCakeTexture.repeat.set(1, 1);
         this.sideCakeMaterial = new THREE.MeshPhongMaterial({ color: "#ffffff", specular: "#000000", emissive: "#000000", shininess: 20, map:  this.sideCakeTexture});
 
         this.cakeInteriorTexture = new THREE.TextureLoader().load("textures/cakeInterior.webp");
@@ -30,12 +30,10 @@ class MyCakeSlice{
         this.cakeInteriorTexture.wrapT = THREE.RepeatWrapping;
         this.cakeInteriorTexture.repeat.set(1, 1);
         this.cakeInteriorMaterial = new THREE.MeshPhongMaterial({ color: "#ffffff", specular: "#000000", emissive: "#000000", shininess: 20, map:  this.cakeInteriorTexture});
-
-        //this.cakeTotalMaterial = new THREE.MeshFaceMaterial([this.cakeMaterial, this.sideCakeMaterial, this.cakeMaterial]);
     }
 
     buildCakeSlice(){
-        this.cakeSliceMesh = this.primitives.buildCylinder(0, 0, 0, 0, 0, 0, 1.25, 1.25, 0.8, 30, 5, false, 0, 2*Math.PI/10, this.cakeMaterial, false);
+        this.cakeSliceMesh = this.primitives.buildCylinder(0, 0, 0, 0, 0, 0, 1.25, 1.25, 0.8, 30, 5, false, 0, 2*Math.PI/10, [this.sideCakeMaterial, this.cakeMaterial, this.cakeMaterial], false);
         let cakeArray = this.primitives.buildCylinderCovers(0, 0, 0, 0, 0, 0, 1.25, 0.8, 0, 2*Math.PI/10, this.cakeInteriorMaterial, false);
         this.cakeSliceCover1 = cakeArray[0];
         this.cakeSliceCover2 = cakeArray[1];
